e905bbca433b2b9e98a37a7e59a073312eca043f
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2021-01-05 14:26:59 -0300

    POST /orders

--- backend/src/main/java/com/pedrofernandes/deliverypf/controllers/OrderController.java
+++ backend/src/main/java/com/pedrofernandes/deliverypf/controllers/OrderController.java
@@ -5,10 +5,9 @@ import com.pedrofernandes.deliverypf.entities.dto.ProductDTO;
 import com.pedrofernandes.deliverypf.service.OrderService;
 import com.pedrofernandes.deliverypf.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +22,11 @@ public class OrderController {
     public ResponseEntity<List<OrderDTO>> findAll(){
         List<OrderDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+        return new ResponseEntity(service.insert(dto), HttpStatus.CREATED);
     }
 
 }
--- backend/src/main/java/com/pedrofernandes/deliverypf/service/OrderService.java
+++ backend/src/main/java/com/pedrofernandes/deliverypf/service/OrderService.java
@@ -4,12 +4,14 @@ import com.pedrofernandes.deliverypf.entities.Order;
 import com.pedrofernandes.deliverypf.entities.Product;
 import com.pedrofernandes.deliverypf.entities.dto.OrderDTO;
 import com.pedrofernandes.deliverypf.entities.dto.ProductDTO;
+import com.pedrofernandes.deliverypf.entities.enums.OrderStatus;
 import com.pedrofernandes.deliverypf.repositories.OrderRepository;
 import com.pedrofernandes.deliverypf.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,9 +21,24 @@ public class OrderService {
     @Autowired
     OrderRepository repo;
 
+    @Autowired
+    ProductRepository productRepository;
+
     @Transactional(readOnly = true)
-    public List<OrderDTO> findAll(){
+    public List<OrderDTO> findAll() {
         List<Order> list = repo.findOrderWithProduct();
         return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO orderDTO) {
+        Order order = new Order(null, orderDTO.getAddress(), orderDTO.getLatitude(), orderDTO.getLongitude(),
+                Instant.now(), OrderStatus.PENDING);
+        for (ProductDTO p : orderDTO.getProducts()) {
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+        order = repo.save(order);
+        return new OrderDTO(order);
     }
 }
