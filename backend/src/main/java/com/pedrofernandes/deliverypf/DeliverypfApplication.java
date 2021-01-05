e31a4bf5c263b0f16ffad22939f45a6741bd24c3
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2021-01-05 11:02:38 -0300

    Project created

---
+++ backend/src/main/java/com/pedrofernandes/deliverypf/DeliverypfApplication.java
@@ -0,0 +1,13 @@
+package com.pedrofernandes.deliverypf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverypfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverypfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pedrofernandes/deliverypf/DeliverypfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrofernandes.deliverypf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverypfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
