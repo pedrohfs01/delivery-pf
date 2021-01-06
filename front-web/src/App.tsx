d0144af2454703d768b178da0718edda1ffe6dd3
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2021-01-06 13:26:05 -0300

    Add navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
   return (
     <div>
-      <h1>hello world</h1>      
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import { ReactComponent as Logo } from "./logo.svg"
+
+function Navbar(){
+    return(
+        <nav className="main-navbar">
+            <Logo></Logo>
+            <a href="#" className="logo-text">Delivery PF</a>
+        </nav>
+
+    );
+}
+
+export default Navbar;
